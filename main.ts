f89c8aa0a9fd3f96ea41d08f0ff25d844f9e7662
yprazzi <72910828+yprazzi@users.noreply.github.com>
2021-02-25 09:06:22 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+let x = 0
 basic.forever(function () {
-	
+    x = 0
+    for (let index = 0; index < 5; index++) {
+        led.plot(x, 0)
+        basic.pause(200)
+        led.toggle(x + 1, 0)
+        basic.pause(200)
+        led.toggle(x + 2, 0)
+        basic.pause(200)
+        led.toggle(x + 3, 0)
+        basic.pause(200)
+        led.toggle(x + 4, 0)
+        basic.pause(200)
+    }
+    x = 4
+    for (let index = 0; index < 5; index++) {
+        led.plot(x, 0)
+        led.toggle(x - 1, 0)
+        basic.pause(500)
+    }
 })
